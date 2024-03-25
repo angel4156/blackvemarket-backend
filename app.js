b79b4074f69120b46e9747ebda0f6638611dd321
angel4156 <angel4156@hotmail.com>
2024-03-25 14:12:14 -0400

    Upload project

---
+++ app.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+const userRoutes = require("./routes/users");
+const productRoutes = require("./routes/products");
+const path = require("path");
+require("dotenv").config();
+const app = express();
+const http = require("http").createServer(app);
+const url = process.env.MONGO_URL;
+app.use(morgan("dev"));
+
+//mongodb connect
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("mongodb connected");
+  })
+  .catch((err) => console.log(err));
+
+app.use(bodyParser.json({limit:"50mb"}));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit:50000 }));
+app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public", "build")));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "build", "index.html"));
+});
+app.use(cors());
+
+//define Routes
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+
+http.listen(3333, () => {
+  console.log(`client started port 3333`);
+});
+app.listen(process.env.PORT || 9999, "0.0.0.0", () => {
+  console.log(`Server started port 9999`);
+});
